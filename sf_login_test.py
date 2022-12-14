0a865846ef872ef3094ee3e5f15c9d136704e7e5
Nigel Hughes <nigel.hughes@sage.com>
2022-12-14 16:25:44 +0000

    ...

---
+++ sf_login_test.py
@@ -0,0 +1,19 @@
+import logging
+import sys
+
+from simple_salesforce import Salesforce
+
+logging.basicConfig(format="%(asctime)s : %(message)s", level=logging.DEBUG)
+
+if __name__ == "__main__":
+    sf = Salesforce(
+        username="test-z1upabcxzffh@example.com",
+        password="",
+        security_token="4MpoIX7SwcHZLZgHslVOFcRU",
+        domain="test",
+    )
+    if not sf:
+        print("\n*** Not Logged into Salesforce ***\n")
+        sys.exit(1)
+
+    print("\n*** Logged into Salesforce ***\n")
